38cf8dbedb68c8b94c2a59ffb04c0156f1d108b5
silvianarosantiy <76066155+silvianarosantiy@users.noreply.github.com>
2020-12-16 12:49:43 +0700

    Add files via upload

---
+++ qgis2web_2020_12_16-12_45_12_305154/data/soloo_1.js
@@ -0,0 +1,1 @@
+var json_soloo_1 = {"type":"FeatureCollection","name":"soloo_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.824266543730985,-7.571039391150204]}}]}
